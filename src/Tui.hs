ec194c28ecb0da8d5f295af3bf697aeb9a48bf53
benjmhart <benjmhart@gmail.com>
2020-03-11 20:07:19 -0400

    stage 2

--- src/Tui.hs
+++ src/Tui.hs
@@ -3,11 +3,19 @@
 module Tui where
 
 import System.Directory
+import System.Exit (die)
+
+import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NE
+
+import qualified Cursor.Simple.List.NonEmpty as NEC
 
 import Brick.AttrMap
 import Brick.Main
 import Brick.Types
+import Brick.Util (fg)
 import Brick.Widgets.Core
+import Graphics.Vty.Attributes (red)
 import Graphics.Vty.Input.Events
 
 tui :: IO ()
@@ -17,12 +25,15 @@ tui = do
   print endState
 
 data TuiState =
-  TuiState { tuiStatePaths :: [FilePath] }
+  TuiState { tuiStatePaths :: NEC.NonEmptyCursor FilePath }
   deriving (Show, Eq)
 
 data ResourceName =
   ResourceName
   deriving (Show, Eq, Ord)
+
+data Selected = NotSel | Sel
+  deriving Eq
 
 tuiApp :: App TuiState e ResourceName
 tuiApp =
@@ -31,19 +42,29 @@ tuiApp =
     , appChooseCursor = showFirstCursor
     , appHandleEvent = handleTuiEvent
     , appStartEvent = pure
-    , appAttrMap = const $ attrMap mempty []
+    , appAttrMap = const $ attrMap mempty [("selected", fg red)]
     }
 
 buildInitialState :: IO TuiState
 buildInitialState = do
   here <- getCurrentDirectory
   contents <- getDirectoryContents here
-  pure $ TuiState contents
+  case NE.nonEmpty contents of
+    Nothing -> die "There are no directory contents"
+    Just ne -> pure $ TuiState $ NEC.makeNonEmptyCursor ne
 
 drawTui :: TuiState -> [Widget ResourceName]
 drawTui (TuiState tuiStatePaths) 
-  = [ vBox $ map str $ tuiStatePaths
+  = [ vBox $ mconcat 
+      [ map (drawPath NotSel) $ reverse $ NEC.nonEmptyCursorPrev tuiStatePaths
+      , [drawPath Sel $ NEC.nonEmptyCursorCurrent tuiStatePaths]
+      , map (drawPath NotSel) $ NEC.nonEmptyCursorNext tuiStatePaths
+      ]
     ]
+
+drawPath :: Selected -> FilePath -> Widget n
+drawPath NotSel fp = str fp
+drawPath Sel fp = withAttr "selected" $ str fp
 
 handleTuiEvent :: TuiState -> BrickEvent n e -> EventM n (Next TuiState)
 handleTuiEvent s e =
@@ -51,5 +72,15 @@ handleTuiEvent s e =
     VtyEvent vtye ->
       case vtye of
         EvKey (KChar 'q') [] -> halt s
+        EvKey KDown [] -> do
+          let nec = tuiStatePaths s
+          case NEC.nonEmptyCursorSelectNext nec of
+            Nothing -> continue s
+            Just nec' -> continue $ s { tuiStatePaths = nec'}
+        EvKey KUp [] -> do
+          let nec = tuiStatePaths s
+          case NEC.nonEmptyCursorSelectPrev nec of
+            Nothing -> continue s
+            Just nec' -> continue $ s { tuiStatePaths = nec'}
         _ -> continue s
     _ -> continue s
